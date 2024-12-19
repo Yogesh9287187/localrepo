29c8e740d8735502b754f5600f18bcb203544738
Yogesh9287187 <yogesh.gorade@outlook.com>
2024-12-19 13:53:29 +0530

    correction

---
+++ providers.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.14.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features { 
+    
+    
+   }
+   subscription_id = "a38b30d2-a252-4b12-a442-badb639e037a"
+}
